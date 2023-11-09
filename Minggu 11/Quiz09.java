3b4c27a4de25c20e7567907247f23b48dbbb13a8
Ericharw <ericharizki12@gmail.com>
2023-11-09 16:14:56 +0700

    modifikasi jawaban no 3

--- Minggu 11/Quiz09.java
+++ Minggu 11/Quiz09.java
@@ -7,8 +7,8 @@ public class Quiz09 {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner input = new Scanner(System.in);
-
         char menu = 'y';
+
         do{
             int number = random.nextInt(10) + 1;
             boolean success = false;
@@ -16,10 +16,15 @@ public class Quiz09 {
                 System.out.print("Tebak angka (1-10): ");
                 int answer = input.nextInt();
                 input.nextLine();
+                if (answer > number) {
+                    System.out.println("Lebih kecil dari ");
+                } else if (answer < number) {
+                    System.out.println("Lebih besar dari ");
+                }
                 success = (answer == number);
             }while(!success);
             System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
             menu = input.nextLine().charAt(0);
         }while(menu=='y' || menu=='Y');
-    }
+    } 
 }
