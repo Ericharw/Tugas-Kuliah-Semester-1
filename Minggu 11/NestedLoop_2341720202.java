39ad7a598e40c4fd0a6a02356ba5b72455e427c6
Ericharw <ericharizki12@gmail.com>
2023-11-09 21:25:07 +0700

    commit

--- Minggu 11/NestedLoop_2341720202.java
+++ Minggu 11/NestedLoop_2341720202.java
@@ -8,7 +8,7 @@ public class NestedLoop_2341720202 {
         double[][] temps = new double[5][7];
 
         for (int i = 0; i < temps.length; i++) {
-            System.out.println("Kota ke-" + i);
+            System.out.println("Kota ke-" + (i+1));
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
                 temps [i][j] = sc.nextDouble();
@@ -24,5 +24,4 @@ public class NestedLoop_2341720202 {
             System.out.println();
         }
     }
-    
 }
