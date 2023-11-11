5f904e9863671140487b056ab3a6b7cc5d72fadf
Ericharw <ericharizki12@gmail.com>
2023-11-11 21:56:21 +0700

    modifikasi untuk menampilkan nama penonton setiap baris

--- Minggu 10/Bioskop09.java
+++ Minggu 10/Bioskop09.java
@@ -31,8 +31,6 @@ public class Bioskop09 {
         System.out.println(penonton[2].length);
         System.out.println(penonton[3].length);
 
-        System.out.println("Penonton pada baris ke-3: ");
-
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(",", penonton[i]));
         }
