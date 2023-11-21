555100ea93a5c70331e78e26e0ce73e0456f1336
Ericharw <ericharizki12@gmail.com>
2023-11-21 16:28:20 +0700

    tugas menghitung volume dan luas permukaan kubus

---
+++ Minggu13/Kubus09.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Kubus09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int s,Lp,vol;
+        System.out.println("Masukkan panjang sisi kubus");
+        s=sc.nextInt();
+
+        Lp=hitungLp(s);
+        System.out.println("Luas permukaan kubus adalah "+Lp);
+
+        vol=hitungVolume(s);
+        System.out.println("volume kubus adalah "+vol);
+    }
+
+    static int hitungLp (int s) {
+        int Lp=6*s*s;
+        return Lp;
+    }
+    static int hitungVolume (int s) {
+        int vol=s*s*s;
+        return vol;
+    }
+}
+
