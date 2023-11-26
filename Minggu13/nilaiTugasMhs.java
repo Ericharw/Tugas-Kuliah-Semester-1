e6fab96d915033b34e961751589b8d8fbe42b62e
Ericharw <ericharizki12@gmail.com>
2023-11-26 16:13:00 +0700

    mengisi array 2 dimensi nilai tugas lima mahasiswa

--- Minggu13/nilaiTugasMhs.java
+++ Minggu13/nilaiTugasMhs.java
@@ -1,43 +1,76 @@
 import java.util.Scanner;
 
 public class nilaiTugasMhs {
-   
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int jmlMhs = 5;
-        int jmlMinggu = 7;
-        double[][] nilaiTugas = new double[jmlMhs][jmlMinggu];
-        String[] namaMhs = new String[jmlMhs];
-
-        inputDataNamaMhs(sc, namaMhs);
-        inputDataNilai(sc, nilaiTugas);
-
-        tampilkanSeluruhNilai(nilaiTugas);
-    }
-    public static void inputDataNamaMhs(Scanner sc, String[] namaMhs) {
-        for (int i = 0; i < namaMhs.length; i++) {
-            System.out.print("Masukkan nama Mahasiswa " + (i+1) + ": ");
-            namaMhs[i] = sc.nextLine();
-        }
-    }
-    public static void inputDataNilai(Scanner sc, double[][] nilaiTugas) {
-        for (int i = 0; i < nilaiTugas.length; i++) {
-            System.out.println("Masukkan nilai tugas untuk Mahasiswa " + (i + 1) + ":");
-            for (int j = 0; j < nilaiTugas[i].length; j++) {
-                System.out.print("Minggu ke-" + (j + 1) + ": ");
-                nilaiTugas[i][j] = sc.nextDouble();
-            }
-        }
-    }
-    public static void tampilkanSeluruhNilai(double[][] nilaiTugas) {
-        System.out.println("\nSeluruh Nilai Mahasiswa:");
-        for (int i = 0; i < nilaiTugas.length; i++) {
-            System.out.print("Mahasiswa " + (i + 1) + ": ");
-            for (int j = 0; j < nilaiTugas[i].length; j++) {
-                System.out.print("Minggu-" + (j + 1) + ": " + nilaiTugas[i][j] + "\t");
+    
+    static void inputNilai(int [][] nilai, String [] nama) {
+        Scanner sc = new Scanner (System.in);
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Nilai dari Mahasiswa "+nama[i]);
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Nilai Minggu ke-"+(j+1)+" : ");
+                nilai[i][j] = sc.nextInt();
             }
             System.out.println();
         }
+    }
+
+    static void tampilNilai(int [][] nilai, String [] nama) {
+        int i = 0;
+        for (int a[] : nilai) {
+            System.out.print("Nilai dari Mahasiswa " + nama[i]+"\t: ");
+            for (int b : a) {
+                System.out.print(b + " ");
+            }
+            System.out.println();
+            i++;
+        }
+    }
+
+    static String cariMhsTertinggi (int [][] nilai, String [] nama) {
+        String NamaTertinggi = null;
+        int maks = nilai[0][0];
+        for (int i = 0; i < nilai.length; i++) {
+            for (int j = 0; j < nilai[i].length; j++) {
+                if (nilai[i][j] > maks) {
+                    maks = nilai [i][j];
+                    NamaTertinggi = nama[i];
+                }
+            }
+        }
+        return NamaTertinggi;
+    }
+
+    static void cariNilaiMaks (int [][] nilai, int []nilaiMaks) {
+        nilaiMaks[0] = nilai[0][0];
+        for (int i = 0; i < nilai.length; i++) {
+            for (int j = 0; j < nilai[i].length; j++) {
+                if (nilai[i][j] > nilaiMaks[0]) {
+                    //NilaiMaks
+                    nilaiMaks[0] = nilai [i][j];
+                    //Minggu nilai maks
+                    nilaiMaks[1] = j+1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int [][] nilaiMhs = new int [5][7];
+        String []namaMhs = {"Sari", "Rina", "Yani", "Dwi", "Lusi"};
+        int []nilaiTertinggi = new int[2];
+
+        //Menginput nilai Mahasiswa
+        inputNilai(nilaiMhs, namaMhs);
+
+        //Mengoutput seluruh nilai Mahasiswa
+        tampilNilai(nilaiMhs, namaMhs);
+
+        //Output Nama Mahasiswa dengan nilai tertinggi
+        String mhsNilaiTertinggi = cariMhsTertinggi(nilaiMhs, namaMhs);
+        System.out.println("\nMahasiswa dengan nilai tertinggi adalah " + mhsNilaiTertinggi);
+
+        //Output nilai tertinggi dan minggu dari nilai tersebut
+        cariNilaiMaks(nilaiMhs, nilaiTertinggi);
+        System.out.println("Nilai tertinggi adalah " +nilaiTertinggi[0] + " di Minggu ke-" + nilaiTertinggi[1]);
     }
 }
