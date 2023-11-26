b7930e10e2a88798078e3df30884eb06e3fac672
Ericharw <ericharizki12@gmail.com>
2023-11-26 22:03:54 +0700

    modif input user, menentukan jumlah mahasiswa dan jumlah tugas

--- Minggu13/nilaiTugasMhs.java
+++ Minggu13/nilaiTugasMhs.java
@@ -2,20 +2,23 @@ import java.util.Scanner;
 
 public class nilaiTugasMhs {
     
-    static void inputNilai(int [][] nilai, String [] nama) {
+    static void inputNamaDanNilai(int [][] nilai, String [] nama) {
         Scanner sc = new Scanner (System.in);
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Nilai dari Mahasiswa "+nama[i]);
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println();
+            System.out.print("Masukkan nama Mahasiswa ke-"+(i+1)+" : ");
+            nama[i] = sc.nextLine();
             for (int j = 0; j < nilai[i].length; j++) {
                 System.out.print("Nilai Minggu ke-"+(j+1)+" : ");
                 nilai[i][j] = sc.nextInt();
             }
-            System.out.println();
+            sc.nextLine();
         }
     }
 
     static void tampilNilai(int [][] nilai, String [] nama) {
         int i = 0;
+        System.out.println();
         for (int a[] : nilai) {
             System.out.print("Nilai dari Mahasiswa " + nama[i]+"\t: ");
             for (int b : a) {
@@ -45,7 +48,7 @@ public class nilaiTugasMhs {
         for (int i = 0; i < nilai.length; i++) {
             for (int j = 0; j < nilai[i].length; j++) {
                 if (nilai[i][j] > nilaiMaks[0]) {
-                    //NilaiMaks
+                    //Nilai maks
                     nilaiMaks[0] = nilai [i][j];
                     //Minggu nilai maks
                     nilaiMaks[1] = j+1;
@@ -55,12 +58,19 @@ public class nilaiTugasMhs {
     }
 
     public static void main(String[] args) {
-        int [][] nilaiMhs = new int [5][7];
-        String []namaMhs = {"Sari", "Rina", "Yani", "Dwi", "Lusi"};
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Masukkan jumlah Mahasiswa\t: ");
+        int jmlMhs = sc.nextInt();
+        System.out.print("Masukkan jumlah nilai\t\t: ");
+        int jmlNilai = sc.nextInt();
+
+        int [][] nilaiMhs = new int [jmlMhs][jmlNilai];
+        String []namaMhs = new String [jmlMhs];
         int []nilaiTertinggi = new int[2];
 
-        //Menginput nilai Mahasiswa
-        inputNilai(nilaiMhs, namaMhs);
+        //Menginput nama dan nilai Mahasiswa
+        inputNamaDanNilai(nilaiMhs, namaMhs);
 
         //Mengoutput seluruh nilai Mahasiswa
         tampilNilai(nilaiMhs, namaMhs);
@@ -72,5 +82,6 @@ public class nilaiTugasMhs {
         //Output nilai tertinggi dan minggu dari nilai tersebut
         cariNilaiMaks(nilaiMhs, nilaiTertinggi);
         System.out.println("Nilai tertinggi adalah " +nilaiTertinggi[0] + " di Minggu ke-" + nilaiTertinggi[1]);
+
     }
 }
