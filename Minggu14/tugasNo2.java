79809b6eada341d6a8eb7920802a0b207ace90ea
Ericharw <ericharizki12@gmail.com>
2023-12-02 19:06:33 +0700

    add -tugas nomor 2

---
+++ Minggu14/tugasNo2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class tugasNo2 {
+    static int penjumlahanRekursif(int n) {
+        if (n == 1) {
+            System.out.print(n + "+");
+            return 1;
+        } else {
+            int hasilPenjumlahan = n + penjumlahanRekursif(n-1);
+            System.out.print(n + "+");
+            return hasilPenjumlahan;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("Masukkan nilai n : ");
+        n = sc.nextInt();
+        
+        int hasilPenjumlahan = penjumlahanRekursif(n);
+        System.out.println("=" + hasilPenjumlahan);
+    }
+}
