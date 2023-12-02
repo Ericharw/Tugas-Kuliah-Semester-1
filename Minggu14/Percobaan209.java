10cd32558ffd4f29231433d0660a29787017385b
Ericharw <ericharizki12@gmail.com>
2023-12-02 19:07:27 +0700

    menambahkan kode program untuk mencetak deret perhitungan pangkat

--- Minggu14/Percobaan209.java
+++ Minggu14/Percobaan209.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
+
 public class Percobaan209 {
-    static int hitungPangkat(int x, int y) {
+      static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return (1);
+            return 1;
         } else {
-            return (x*hitungPangkat(x, y-1));
+            System.out.print(x + "x");
+            return x * hitungPangkat(x, y - 1);
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int bilangan, pangkat;
@@ -14,6 +17,9 @@ public class Percobaan209 {
         bilangan = sc.nextInt();
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
-        System.out.println(hitungPangkat(bilangan, pangkat));
+
+        System.out.print("Deret perhitungan: ");
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.println("1 = " + hasil);
     }
 }
